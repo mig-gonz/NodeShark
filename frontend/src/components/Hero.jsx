4536caca62522927f385dea1336e47fc4074fd74
Roman M <rmarchen95@gmail.com>
2023-07-02 13:49:08 -0700

    Update Hero.jsx
    finishing touches on hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,8 +5,8 @@ function Hero() {
     <div
       className="hero min-h-[80vh] bg-right-top bg-[url('https://images.unsplash.com/photo-1546483875-ad9014c88eba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1382&q=80')]  "
     >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content ">
+      <div className="hero-overlay bg-opacity-50"></div>
+      <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">NODE SHARK</h1>
           <p className="mb-5">
