980d7d82853e8006d8d03a736f66d9d14f736649
Roman M <rmarchen95@gmail.com>
2023-07-02 13:31:41 -0700

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,17 +3,14 @@ import React from "react";
 function Hero() {
   return (
     <div
-      className="hero min-h-screen bg-[url('https://plus.unsplash.com/premium_photo-1680742508463-acb8c21af3bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1769&q=80')]  "
-      // style="background-image: url(/images/stock/photo-1507358522600-9f71e620c44e.jpg);"
+      className="hero min-h-[80vh] bg-right-top bg-[url('https://images.unsplash.com/photo-1546483875-ad9014c88eba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1382&q=80')]  "
     >
       <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
+      <div className="hero-content text-center text-neutral-content ">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">NODE SHARK</h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+           Giving software devs the best experience during their physical and mental workouts.  Whether you're coding a marathon or running one, we've got you covered.
           </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
