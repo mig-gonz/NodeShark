a7bbd136242d92073e0e52729e493f2e8f5f0eeb
mig-gonz <mig256@outlook.com>
2023-07-06 15:45:49 -0700

    refactored code in AllProducts, Details, Gallery, Navbar. Fixed Gallery, it is now responsive

--- frontend/src/Components/Details.jsx
+++ frontend/src/Components/Details.jsx
@@ -13,11 +13,16 @@ const Details = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    fetch(`http://localhost:9000/products/${id}`)
-      .then((res) => res.json())
-      .then(({ data }) => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(`http://localhost:9000/products/${id}`);
+        const { data } = await response.json();
         setProduct(data);
-      });
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProducts();
   }, [id]);
 
   if (!product) {
--- frontend/src/Components/Gallery.jsx
+++ frontend/src/Components/Gallery.jsx
@@ -5,18 +5,42 @@ import { Link } from "react-router-dom";
 
 const Gallery = () => {
   const [products, setProducts] = useState([]);
+  const [isMobile, setIsMobile] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
-    fetch("http://localhost:9000/products")
-      .then((res) => res.json())
-      .then(({ data }) => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:9000/products");
+        const { data } = await response.json();
         setProducts(data);
-      });
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProducts();
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1027);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
-  const latestProdcuts = products.slice(0, 8);
-  // I added this because some of the images are larger than others and it didn't look flush with the others.
-  // It only gathers the first 8 images
+  const latestProducts = products.slice(0, 8);
+
+  const handlePrevSlide = () => {
+    setCurrentSlide((prevSlide) => prevSlide - 1);
+  };
+
+  const handleNextSlide = () => {
+    setCurrentSlide((prevSlide) => prevSlide + 1);
+  };
 
   return (
     <div className="bg-white mb-8 flex justify-center items-center overflow-hidden h-[750px]">
@@ -25,38 +49,88 @@ const Gallery = () => {
           Check out our newest drops!
         </h2>
 
-        {/* The carousel isn't formattered for mobile so we should fix that. */}
-        <Carousel
-          showThumbs={false}
-          showStatus={false}
-          centerMode={true}
-          centerSlidePercentage={33.3}
-          autoPlay={true}
-          infiniteLoop={true}
-          showArrows={true}
-          showIndicators={true}
-          interval={3000}
-        >
-          {latestProdcuts.map((product) => (
-            <div key={product.id} className="m-5 p-5 text-[#9c9ea2]">
-              <Link to={`/products/${product.id}`}>
-                <div>
-                  <img
-                    src={product?.Images[0]?.url}
-                    alt=""
-                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                  />
-                </div>
-              </Link>
-              <Link to={`/products/${product.id}`}>
-                <h1 className="text-lg font-bold pl-2 my-2 hover:underline hover:text-[#D0D3DB]">
-                  {product?.name}
-                </h1>
-              </Link>
-              <p className="pl-2 mb-10">{`$${product.price}`}</p>
-            </div>
-          ))}
-        </Carousel>
+        {isMobile ? (
+          <div className="flex overflow-x-auto">
+            {latestProducts.map((product, index) => (
+              <div
+                key={product.id}
+                className={`m-5 p-5 text-[#9c9ea2] ${
+                  index !== currentSlide ? "hidden" : ""
+                }`}
+              >
+                <Link to={`/products/${product.id}`}>
+                  <div>
+                    <img
+                      src={product?.Images[0]?.url}
+                      alt=""
+                      className="h-96 w-full object-cover object-center lg:h-full lg:w-full"
+                    />
+                  </div>
+                </Link>
+                <Link to={`/products/${product.id}`}>
+                  <h1 className="text-lg font-bold pl-2 my-2 hover:underline hover:text-[#D0D3DB]">
+                    {product?.name}
+                  </h1>
+                </Link>
+                <p className="pl-2 mb-10">{`$${product.price}`}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <Carousel
+            showThumbs={false}
+            showStatus={false}
+            centerMode={true}
+            centerSlidePercentage={33.3}
+            autoPlay={true}
+            infiniteLoop={true}
+            showArrows={true}
+            showIndicators={true}
+            interval={3000}
+            selectedItem={currentSlide}
+            onChange={(index) => setCurrentSlide(index)}
+          >
+            {latestProducts.map((product) => (
+              <div key={product.id} className="m-5 p-5 text-[#9c9ea2]">
+                <Link to={`/products/${product.id}`}>
+                  <div>
+                    <img
+                      src={product?.Images[0]?.url}
+                      alt=""
+                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                    />
+                  </div>
+                </Link>
+                <Link to={`/products/${product.id}`}>
+                  <h1 className="text-lg font-bold pl-2 my-2 hover:underline hover:text-[#D0D3DB]">
+                    {product?.name}
+                  </h1>
+                </Link>
+                <p className="pl-2 mb-10">{`$${product.price}`}</p>
+              </div>
+            ))}
+          </Carousel>
+        )}
+
+        {isMobile && (
+          <div className="flex justify-center">
+            {currentSlide !== 0 && (
+              <button
+                className="btn-primary mr-2 hover:bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                onClick={handlePrevSlide}
+              >
+                Previous
+              </button>
+            )}
+            <button
+              className="btn-primary mr-2 hover:bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              onClick={handleNextSlide}
+              disabled={currentSlide === latestProducts.length - 1}
+            >
+              Next
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
