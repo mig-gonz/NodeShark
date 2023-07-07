ba0dd039ef78ba65da47dddef9746bb6c2ca1017
mig-gonz <mig256@outlook.com>
2023-07-07 12:26:04 -0700

    added conditional for wishlist button only to appear when user is logged in

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -204,22 +204,6 @@ const NavBar = () => {
               </div>
               <div className="ml-auto flex items-center">
                 {loginActions}
-                {/* currency */}
-                {/*
-                <div className="hidden lg:ml-8 lg:flex">
-                  <a
-                    href="#"
-                    className="flex items-center text-gray-700 hover:text-gray-800"
-                  >
-                    <img
-                      src="https://tailwindui.com/img/flags/flag-canada.svg"
-                      alt=""
-                      className="block h-auto w-5 flex-shrink-0"
-                    />
-                    <span className="ml-3 block text-sm font-medium">CAD</span>
-                    <span className="sr-only">, change currency</span>
-                  </a>
-                </div> */}
 
                 {/* Search */}
                 <div className="flex lg:ml-6">
@@ -232,22 +216,24 @@ const NavBar = () => {
                   </button>
                 </div>
 
-                {/* Cart */}
-                <div className="ml-4 flow-root lg:ml-6">
-                  <Link
-                    to="/user/wishlist"
-                    className="group -m-2 flex items-center p-2"
-                  >
-                    <ShoppingBagIcon
-                      className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                      aria-hidden="true"
-                    />
-                    <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                      0
-                    </span>
-                    <span className="sr-only">items in cart, view bag</span>
-                  </Link>
-                </div>
+                {/* wishlist */}
+                {currentUser && (
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Link
+                      to="/user/wishlist"
+                      className="group -m-2 flex items-center p-2"
+                    >
+                      <ShoppingBagIcon
+                        className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                        0
+                      </span>
+                      <span className="sr-only">items in cart, view bag</span>
+                    </Link>
+                  </div>
+                )}
               </div>
             </div>
           </div>
