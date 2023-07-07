31956326ebea2ad491e64609dffd3f907f652a24
mig-gonz <mig256@outlook.com>
2023-07-06 19:19:09 -0700

    fixed imports

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Gallery from "../Components/Gallery";
-import Hero from "../Components/Hero";
-import BotHero from "../Components/BotHero";
+import Gallery from "../components/Gallery";
+import Hero from "../components/Hero";
+import BotHero from "../components/BotHero";
 
 const Home = () => {
   return (
