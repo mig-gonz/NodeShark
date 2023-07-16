d8fbd148b4bc833c1b05cf04196082995a78af38
mig-gonz <mig256@outlook.com>
2023-07-16 14:57:27 -0700

    changed the promo content on nav bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -157,7 +157,7 @@ const NavBar = () => {
       <header className="relative bg-white">
         {/* speacial offer */}
         <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
-          Get free delivery on orders over $100
+          Discover the latest trends in gym gear!
         </p>
 
         <nav
