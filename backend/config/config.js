04f782659de00f51cf02446620092fcc58f5274f
mig-gonz <mig256@outlook.com>
2023-07-16 17:53:45 -0700

    added important notice pop up to warn the purpose of this application and to not use real emails or passwords

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,6 +6,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    logging: true,
     dialect: "postgres",
   },
   test: {
