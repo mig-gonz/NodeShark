85e2a8ec1fae5b1031ecba9320ee6b0d04eebb1f
mig-gonz <mig256@outlook.com>
2023-08-01 12:54:57 -0700

    styled clerk sign in and sign up

--- frontend/src/pages/ClerkSignIn.jsx
+++ frontend/src/pages/ClerkSignIn.jsx
@@ -2,10 +2,8 @@ import { SignIn } from "@clerk/clerk-react";
 
 export default function LogIn() {
   return (
-    <div className="md:w-full">
-      <div className="m-auto md:w-1/3 mt-5 mb-5">
-        <SignIn />
-      </div>
+    <div className="flex justify-center">
+      <SignIn />
     </div>
   );
 }
